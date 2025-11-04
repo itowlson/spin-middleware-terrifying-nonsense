dc96a7b68451dabcf35b66784d7bc09a084b7869
itowlson <ivan.towlson@fermyon.com>
2025-11-04 13:38:40 +1300

    `unreachable!` error when service reads body

--- middlybiddly/src/lib.rs
+++ middlybiddly/src/lib.rs
@@ -11,97 +11,40 @@ wit_bindgen::generate!({
 use spin_sdk::http_wasip3::{IntoRequest, IntoResponse, Request};
 
 #[spin_sdk::http_wasip3::http_service]
-async fn handle(mut request: Request) -> impl IntoResponse {
-    munge(&mut request);
+async fn handle(request: Request) -> impl IntoResponse {
+    let request = munge(request);
     spin::up3_5_0::next::handle(request.into_request().unwrap()).await
 }
 
-fn munge(request: &mut Request) {
-    request.headers_mut().append("my-fake-auth-header", http::HeaderValue::from_static("HOLY COW IT WORKS"));
-    // mmm, forbidden header
-    request.headers_mut().remove("connection");
-    request.headers_mut().remove("host");
+fn munge(request: Request) -> Request {
+    let (mut parts, body) = request.into_parts();
 
-    // wat? how does og modify request body? why does request body have headers?
-    // let bod = request.body_mut();
-    // if let Some(bodd) = bod.take_unstarted() {
-    //     bodd.get_headers();
-    // }
+    parts.headers.append("my-fake-auth-header", http::HeaderValue::from_static("HOLY COW IT WORKS"));
+    // mmm, forbidden header
+    parts.headers.remove("connection");
+    parts.headers.remove("host");
+
+    Request::from_parts(parts, body)
 }
 
+// fn munge(request: Request) -> Request {
+//     let (mut parts, body) = request.into_parts();
 
-// wit_bindgen::generate!({
-//     path: "../wit",
-//     world: "spin:up/http-middleware@3.5.0",
-//     async: true,
-//     generate_all,
-// });
+//     parts.headers.append("my-fake-auth-header", http::HeaderValue::from_static("HOLY COW IT WORKS"));
+//     // mmm, forbidden header
+//     parts.headers.remove("connection");
+//     parts.headers.remove("host");
 
-// use wasi::http0_3_0_rc_2025_09_16::types::{Request, Response, ErrorCode};
+//     // use http_body_util::BodyExt;
 
-// struct Guest;
+//     // println!("map_frame");
+//     // let bod2 = body.map_frame(|f| { println!("{:?}", f.data_ref().map(|d| d.len())); f });
+//     // println!("into_inner");
+//     // let bod3 = bod2.into_inner();
+//     // println!("from_parts");
+//     // let rr = Request::from_parts(parts, bod3);
+//     // println!("ret");
+//     // rr
 
-// impl exports::wasi::http0_3_0_rc_2025_09_16::handler::Guest for Guest {
-//     #[allow(async_fn_in_trait)]
-//     async fn handle(request: Request) -> Result<Response,ErrorCode> {
-//         handle_middlybiddly(request).await
-//     }
-// }
-
-// export!(Guest);
-
-// async fn handle_middlybiddly(request: Request) -> Result<Response, ErrorCode> {
-//     // Requests are immutable so yes this is how it has to be done.
-//     let method = request.get_method().await;
-//     let scheme = request.get_scheme().await;
-//     let authority = request.get_authority().await;
-
-//     let pq = request.get_path_with_query().await;
-
-//     let opts = request.get_options().await;
-
-//     let headers = request.get_headers().await.clone().await;
-//     headers.append("my-fake-auth-header".to_string(), "IT WORKS, LOOK".as_bytes().to_vec()).await.unwrap();
-
-//     let (rx, trailers) =
-//         // request.consume_body().await.unwrap();
-//         Request::consume_body(request, wit_future::new(|| Ok(())).1).await;
-
-//     let outgoing_request =
-//         Request::new(headers, Some(rx), trailers, opts).await.0;
-
-//     outgoing_request.set_method(method).await.unwrap();
-//     outgoing_request
-//         .set_path_with_query(pq)
-//         .await
-//         .unwrap();
-//     outgoing_request
-//         .set_scheme(scheme)
-//         .await
-//         .unwrap();
-//     outgoing_request
-//         .set_authority(authority)
-//         .await
-//         .unwrap();
-
-//     spin::up3_5_0::next::handle(outgoing_request).await
-
-//     //    let m = req.get_method().await;
-//     // let h = req.get_headers().await; // .await.append("arse".to_string(), "biscuit".as_bytes().to_vec()).await.unwrap();
-//     // let (b, bfr) = req.consume_body().await.unwrap();
-//     // let opts = req.get_options().await;
-//     // let au = req.get_authority().await;
-
-//     // let (req2, fr) = Request::new(h, b, trailers, opts).await;
-
-//     // spin::up3_5_0::next::handle(req).await
-//     // for (name, value) in req.get_headers().await.copy_all().await {
-//     //     println!("HEADER: {name}={}", String::from_utf8_lossy(&value));
-//     // }
-
-//     // let (_wr, trailers) = wit_future::new(|| Ok(None));
-
-//     // let (resp, _fr) = Response::new(Fields::new().await, None, trailers).await;
-
-//     // Ok(resp)
+//     Request::from_parts(parts, body)
 // }
--- src/lib.rs
+++ src/lib.rs
@@ -6,5 +6,20 @@ async fn handle(request: Request) -> impl IntoResponse {
         println!("HEADER: {name}={}", String::from_utf8_lossy(value.as_bytes()));
     }
 
-    "done\n"
+    use spin_sdk::http_wasip3::body::IncomingBodyExt;
+
+    println!("BIZ LOGIC COMMENCING");
+
+    let response = if let Ok(by) = request.into_body().bytes().await {
+        let text = String::from_utf8_lossy(&by);
+        format!("-- INBOUND MESSAGE --\n{text}\n------------------\n")
+    } else {
+        "done\n".to_string()
+    };
+
+    println!("BIZ LOGIC RESPONDING");
+
+    response
+
+    // "done\n"
 }
