1a71abb51a50a1a702bb5ff9ef08d9945f74799f
itowlson <ivan.towlson@fermyon.com>
2026-02-03 09:42:15 +1300

    Update to Jan 2026 WASI RC

--- middlybiddly/src/lib.rs
+++ middlybiddly/src/lib.rs
@@ -1,9 +1,9 @@
 wit_bindgen::generate!({
     path: "../wit",
-    world: "spin:up/http-middleware@3.5.0",
+    world: "wasi:http/middleware@0.3.0-rc-2026-01-06",
     async: true,
     with: {
-        "wasi:http/types@0.3.0-rc-2025-09-16": spin_sdk::http_wasip3::wasip3::http::types,
+        "wasi:http/types@0.3.0-rc-2026-01-06": spin_sdk::http_wasip3::wasip3::http::types,
     },
     generate_all,
 });
@@ -13,10 +13,10 @@ use spin_sdk::http_wasip3::{IntoRequest, IntoResponse, Request};
 #[spin_sdk::http_wasip3::http_service]
 async fn handle(request: Request) -> impl IntoResponse {
     let request = munge(request).await;
-    spin::up3_5_0::next::handle(request.into_request().unwrap()).await
+    wasi::http0_3_0_rc_2026_01_06::handler::handle(request.into_request().unwrap()).await
 }
 
-async fn munge(request: Request) -> http::Request<impl http_body::Body<Data = bytes::Bytes, Error = spin_sdk::http_wasip3::wasip3::http::handler::ErrorCode>> {
+async fn munge(request: Request) -> http::Request<impl http_body::Body<Data = bytes::Bytes, Error = spin_sdk::http_wasip3::wasip3::http::types::ErrorCode>> {
     let (mut parts, body) = request.into_parts();
 
     parts.headers.append("my-fake-auth-header", http::HeaderValue::from_static("HOLY COW IT WORKS"));
--- middlybiddly2/src/lib.rs
+++ middlybiddly2/src/lib.rs
@@ -1,9 +1,9 @@
 wit_bindgen::generate!({
     path: "../wit",
-    world: "spin:up/http-middleware@3.5.0",
+    world: "wasi:http/middleware@0.3.0-rc-2026-01-06",
     async: true,
     with: {
-        "wasi:http/types@0.3.0-rc-2025-09-16": spin_sdk::http_wasip3::wasip3::http::types,
+        "wasi:http/types@0.3.0-rc-2026-01-06": spin_sdk::http_wasip3::wasip3::http::types,
     },
     generate_all,
 });
@@ -13,7 +13,7 @@ use spin_sdk::http_wasip3::{IntoRequest, IntoResponse, Request};
 #[spin_sdk::http_wasip3::http_service]
 async fn handle(mut request: Request) -> impl IntoResponse {
     munge(&mut request);
-    spin::up3_5_0::next::handle(request.into_request().unwrap()).await
+    wasi::http0_3_0_rc_2026_01_06::handler::handle(request.into_request().unwrap()).await
 }
 
 fn munge(request: &mut Request) {
